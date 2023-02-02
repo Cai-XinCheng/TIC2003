f4f28352b20f7540b7c3809d8533c04ef48dfd89
Yongjie Ma <20391402+yongjiema@users.noreply.github.com>
2023-02-02 21:50:51 +0800

    Add comments

--- source/SQLiteWrapper.h
+++ source/SQLiteWrapper.h
@@ -9,92 +9,128 @@ using namespace std;
 
 class SQLiteWrapper {
 public:
+    // destructor
     ~SQLiteWrapper();
 
+    // method to open to the database connection
     void open(const string& database);
 
+    // method to close the database connection
     void close();
 
+    // method to excute a query without return
     void execute(const string& sql);
 
+    // method to excute a parameterized query without return
     template<typename... Args>
     void execute(const string& sql, Args&&... args);
 
+    // method to excute a query and returns query result
     template<typename... Types>
     vector<tuple<Types...>> select(const string& sql);
 
+    // method to excute a query and returns query result
     template<typename... Types>
     void select(vector<tuple<Types...>>& dbResults, const string& sql);
 
+    // method to excute a parameterized query and returns query result
     template<typename... Types, typename... Args>
     vector<tuple<Types...>> select(const string& sql, Args&&... args);
 
+    // method to excute a parameterized query and returns query result
     template<typename... Types, typename... Args>
     void select(vector<tuple<Types...>>& dbResults, const string& sql, Args&&... args);
 
+    // method to excute a query and returns the first column of the query result
     template<typename T>
     vector<T> selectFirstColumn(const string& sql);
 
+    // method to excute a query and returns the first column of the query result
     template<typename T>
     void selectFirstColumn(vector<T>& dbResults, const string& sql);
 
+    // method to excute a parameterized query and returns the first column of the query result
     template<typename T, typename... Args>
     vector<T> selectFirstColumn(const string& sql, Args&&... args);
 
+    // method to excute a parameterized query and returns the first column of the query result
     template<typename T, typename... Args>
     void selectFirstColumn(vector<T>& dbResults, const string& sql, Args&&... args);
 
 private:
+    // the connection pointer to the database
     sqlite3* db;
 
+    // method to ensure SQLite result code is non-error
     static void ensureNonerror(sqlite3* db, int resultCode);
 
+    // method to ensure SQLite result code is non-error
     static void ensureNonerror(sqlite3_stmt* statement, int resultCode);
 
+    // method to prepare a SQLite statement
     static void prepare(sqlite3* db, const string& sql, sqlite3_stmt** statement);
 
+    // method to bind int32 parameter
     static void bindParameter(sqlite3_stmt* preparedStatement, int index, const int32_t aValue);
 
+    // method to bind uint32 parameter
     static void bindParameter(sqlite3_stmt* preparedStatement, int index, const uint32_t aValue);
 
+    // method to bind int64 parameter
     static void bindParameter(sqlite3_stmt* preparedStatement, int index, const int64_t aValue);
 
+    // method to bind dobule parameter
     static void bindParameter(sqlite3_stmt* preparedStatement, int index, const double aValue);
 
+    // method to bind string parameter
     static void bindParameter(sqlite3_stmt* preparedStatement, int index, const string& value);
 
+    // method to bind char* parameter
     static void bindParameter(sqlite3_stmt* preparedStatement, int index, const char* value);
 
+    // method to bind parameters
     template<typename T, typename... Args>
     static void bindParameters(sqlite3_stmt* preparedStatement, int index, T&& first);
 
+    // method to bind parameters
     template<typename T, typename... Args>
     static void bindParameters(sqlite3_stmt* preparedStatement, int index, T&& first, Args&&... args);
 
+    // method to call SQLite step function and returns whether still contains data
     static bool step(sqlite3_stmt* preparedStatement);
 
+    // method to get a column value as int32
     static void getColumn(sqlite3_stmt* preparedStatement, int index, int32_t& value);
 
+    // method to get a column value as uint32
     static void getColumn(sqlite3_stmt* preparedStatement, int index, uint32_t& value);
 
+    // method to get a column value as int64
     static void getColumn(sqlite3_stmt* preparedStatement, int index, int64_t& value);
 
+    // method to get a column value as double
     static void getColumn(sqlite3_stmt* preparedStatement, int index, double& value);
 
+    // method to get a column value as string
     static void getColumn(sqlite3_stmt* preparedStatement, int index, string& value);
 
+    // method to get a row
     template<typename T>
     static tuple<T> getRow(sqlite3_stmt* preparedStatement, int columnIndex = 0);
 
+    // method to get a row
     template<typename T1, typename T2, typename... Types>
     static tuple<T1, T2, Types...> getRow(sqlite3_stmt* preparedStatement, int columnIndex = 0);
 
+    // method to get all rows with all columns
     template<typename... Types>
     static void getRowsWithAllColumns(sqlite3_stmt* preparedStatement, vector<tuple<Types...>>& dbResults);
 
+    // method to get all rows with the first column only
     template<typename T>
     static void getRowsWithFirstColumn(sqlite3_stmt* preparedStatement, vector<T>& dbResults);
 
+    // method to finalize SQLite statement
     static void finalize(sqlite3_stmt* preparedStatement);
 };
 
--- source/SQLiteWrapper.hpp
+++ source/SQLiteWrapper.hpp
@@ -3,10 +3,12 @@
 
 #include "SQLiteWrapper.h"
 
+// destructor
 inline SQLiteWrapper::~SQLiteWrapper() {
     sqlite3_close(db);
 }
 
+// method to open to the database connection
 inline void SQLiteWrapper::open(const string& database) {
     const int ret = sqlite3_open(database.c_str(), &db);
     if (ret != SQLITE_OK) {
@@ -14,6 +16,7 @@ inline void SQLiteWrapper::open(const string& database) {
     }
 }
 
+// method to close the database connection
 inline void SQLiteWrapper::close() {
     int ret = sqlite3_close(db);
     if (ret != SQLITE_OK) {
@@ -21,6 +24,7 @@ inline void SQLiteWrapper::close() {
     }
 }
 
+// method to excute a query without return
 inline void SQLiteWrapper::execute(const string& sql) {
     sqlite3_stmt* statement = nullptr;
 
@@ -37,6 +41,7 @@ inline void SQLiteWrapper::execute(const string& sql) {
     finalize(statement);
 }
 
+// method to excute a parameterized query without return
 template<typename... Args>
 inline void SQLiteWrapper::execute(const string& sql, Args&&... args) {
     sqlite3_stmt* statement = nullptr;
@@ -55,6 +60,7 @@ inline void SQLiteWrapper::execute(const string& sql, Args&&... args) {
     finalize(statement);
 }
 
+// method to excute a query and returns query result
 template<typename... Types>
 inline vector<tuple<Types...>> SQLiteWrapper::select(const string& sql) {
     vector<tuple<Types...>> dbResults;
@@ -63,6 +69,7 @@ inline vector<tuple<Types...>> SQLiteWrapper::select(const string& sql) {
     return dbResults;
 }
 
+// method to excute a query and returns query result
 template<typename... Types>
 inline void SQLiteWrapper::select(vector<tuple<Types...>>& dbResults, const string& sql) {
     sqlite3_stmt* statement = nullptr;
@@ -80,6 +87,7 @@ inline void SQLiteWrapper::select(vector<tuple<Types...>>& dbResults, const stri
     finalize(statement);
 }
 
+// method to excute a parameterized query and returns query result
 template<typename... Types, typename... Args>
 inline vector<tuple<Types...>> SQLiteWrapper::select(const string& sql, Args&&... args) {
     vector<tuple<Types...>> dbResults;
@@ -88,6 +96,7 @@ inline vector<tuple<Types...>> SQLiteWrapper::select(const string& sql, Args&&..
     return dbResults;
 }
 
+// method to excute a parameterized query and returns query result
 template<typename... Types, typename... Args>
 inline void SQLiteWrapper::select(vector<tuple<Types...>>& dbResults, const string& sql, Args&&... args) {
     sqlite3_stmt* statement = nullptr;
@@ -106,6 +115,7 @@ inline void SQLiteWrapper::select(vector<tuple<Types...>>& dbResults, const stri
     finalize(statement);
 }
 
+// method to excute a query and returns the first column of the query result
 template<typename T>
 inline vector<T> SQLiteWrapper::selectFirstColumn(const string& sql) {
     vector<T> dbResults;
@@ -114,6 +124,7 @@ inline vector<T> SQLiteWrapper::selectFirstColumn(const string& sql) {
     return dbResults;
 }
 
+// method to excute a query and returns the first column of the query result
 template<typename T>
 inline void SQLiteWrapper::selectFirstColumn(vector<T>& dbResults, const string& sql) {
     sqlite3_stmt* statement = nullptr;
@@ -131,6 +142,7 @@ inline void SQLiteWrapper::selectFirstColumn(vector<T>& dbResults, const string&
     finalize(statement);
 }
 
+// method to excute a parameterized query and returns the first column of the query result
 template<typename T, typename... Args>
 inline vector<T> SQLiteWrapper::selectFirstColumn(const string& sql, Args&&... args) {
     vector<T> dbResults;
@@ -139,6 +151,7 @@ inline vector<T> SQLiteWrapper::selectFirstColumn(const string& sql, Args&&... a
     return dbResults;
 }
 
+// method to excute a parameterized query and returns the first column of the query result
 template<typename T, typename... Args>
 inline void SQLiteWrapper::selectFirstColumn(vector<T>& dbResults, const string& sql, Args&&... args) {
     sqlite3_stmt* statement = nullptr;
@@ -157,6 +170,7 @@ inline void SQLiteWrapper::selectFirstColumn(vector<T>& dbResults, const string&
     finalize(statement);
 }
 
+// method to ensure SQLite result code is non-error
 inline void SQLiteWrapper::ensureNonerror(sqlite3* db, int resultCode) {
     // https://www.sqlite.org/rescode.html#result_codes_versus_error_codes
     if (resultCode != SQLITE_OK && resultCode != SQLITE_ROW && resultCode != SQLITE_DONE) {
@@ -165,57 +179,68 @@ inline void SQLiteWrapper::ensureNonerror(sqlite3* db, int resultCode) {
     }
 }
 
+// method to ensure SQLite result code is non-error
 inline void SQLiteWrapper::ensureNonerror(sqlite3_stmt* statement, int resultCode) {
     sqlite3* db = sqlite3_db_handle(statement);
     ensureNonerror(db, resultCode);
 }
 
+// method to prepare a SQLite statement
 inline void SQLiteWrapper::prepare(sqlite3* db, const string& sql, sqlite3_stmt** statement) {
     int ret = sqlite3_prepare_v2(db, sql.c_str(), static_cast<int>(sql.size()), statement, nullptr);
     ensureNonerror(db, ret);
 }
 
+// method to bind int32 parameter
 inline void SQLiteWrapper::bindParameter(sqlite3_stmt* preparedStatement, int index, const int32_t value) {
     const int ret = sqlite3_bind_int(preparedStatement, index, value);
     ensureNonerror(preparedStatement, ret);
 }
 
+// method to bind uint32 parameter
 inline void SQLiteWrapper::bindParameter(sqlite3_stmt* preparedStatement, int index, const uint32_t value) {
     const int ret = sqlite3_bind_int64(preparedStatement, index, value);
     ensureNonerror(preparedStatement, ret);
 }
 
+// method to bind int64 parameter
 inline void SQLiteWrapper::bindParameter(sqlite3_stmt* preparedStatement, int index, const int64_t value) {
     const int ret = sqlite3_bind_int64(preparedStatement, index, value);
     ensureNonerror(preparedStatement, ret);
 }
 
+// method to bind dobule parameter
 inline void SQLiteWrapper::bindParameter(sqlite3_stmt* preparedStatement, int index, const double value) {
     const int ret = sqlite3_bind_double(preparedStatement, index, value);
     ensureNonerror(preparedStatement, ret);
 }
 
+// method to bind string parameter
 inline void SQLiteWrapper::bindParameter(sqlite3_stmt* preparedStatement, int index, const string& value) {
     const int ret = sqlite3_bind_text(preparedStatement, index, value.c_str(), static_cast<int>(value.size()), SQLITE_TRANSIENT);
     ensureNonerror(preparedStatement, ret);
 }
 
+// method to bind char* parameter
 inline void SQLiteWrapper::bindParameter(sqlite3_stmt* preparedStatement, int index, const char* value) {
     const int ret = sqlite3_bind_text(preparedStatement, index, value, -1, SQLITE_TRANSIENT);
     ensureNonerror(preparedStatement, ret);
 }
 
+// method to bind parameters
 template<typename T, typename... Args>
 inline void SQLiteWrapper::bindParameters(sqlite3_stmt* preparedStatement, int index, T&& first) {
     bindParameter(preparedStatement, index, first);
 }
 
+// method to bind parameters
 template<typename T, typename... Args>
 inline void SQLiteWrapper::bindParameters(sqlite3_stmt* preparedStatement, int index, T&& first, Args&&... args) {
     bindParameter(preparedStatement, index, first);
     bindParameters(preparedStatement, index + 1, forward<Args>(args)...);
 }
 
+// method to call SQLite step function and returns whether still contains data
 inline bool SQLiteWrapper::step(sqlite3_stmt* preparedStatement) {
     const int ret = sqlite3_step(preparedStatement);
     ensureNonerror(preparedStatement, ret);
@@ -223,26 +248,32 @@ inline bool SQLiteWrapper::step(sqlite3_stmt* preparedStatement) {
     return ret == SQLITE_ROW;
 }
 
+// method to get a column value as int32
 inline void SQLiteWrapper::getColumn(sqlite3_stmt* preparedStatement, int index, int32_t& value) {
     value = sqlite3_column_int(preparedStatement, index);
 }
 
+// method to get a column value as uint32
 inline void SQLiteWrapper::getColumn(sqlite3_stmt* preparedStatement, int index, uint32_t& value) {
     value = static_cast<unsigned>(sqlite3_column_int64(preparedStatement, index));
 }
 
+// method to get a column value as int64
 inline void SQLiteWrapper::getColumn(sqlite3_stmt* preparedStatement, int index, int64_t& value) {
     value = sqlite3_column_int64(preparedStatement, index);
 }
 
+// method to get a column value as double
 inline void SQLiteWrapper::getColumn(sqlite3_stmt* preparedStatement, int index, double& value) {
     value = sqlite3_column_double(preparedStatement, index);
 }
 
+// method to get a column value as string
 inline void SQLiteWrapper::getColumn(sqlite3_stmt* preparedStatement, int index, string& value) {
     value = string(reinterpret_cast<const char*>(sqlite3_column_text(preparedStatement, index)));
 }
 
+// method to get a row
 template<typename T>
 inline tuple<T> SQLiteWrapper::getRow(sqlite3_stmt* preparedStatement, int columnIndex) {
     T value;
@@ -251,6 +282,7 @@ inline tuple<T> SQLiteWrapper::getRow(sqlite3_stmt* preparedStatement, int colum
     return tie(value);
 }
 
+// method to get a row
 template<typename T1, typename T2, typename... Types>
 inline tuple<T1, T2, Types...> SQLiteWrapper::getRow(sqlite3_stmt* preparedStatement, int columnIndex) {
     T1 value;
@@ -259,6 +291,7 @@ inline tuple<T1, T2, Types...> SQLiteWrapper::getRow(sqlite3_stmt* preparedState
     return tuple_cat(tie(value), getRow<T2, Types...>(preparedStatement, columnIndex + 1));
 }
 
+// method to get all rows with all columns
 template<typename... Types>
 inline void SQLiteWrapper::getRowsWithAllColumns(sqlite3_stmt* preparedStatement, vector<tuple<Types...>>& dbResults) {
     int columnsCount = -1;
@@ -272,6 +305,7 @@ inline void SQLiteWrapper::getRowsWithAllColumns(sqlite3_stmt* preparedStatement
     }
 }
 
+// method to get all rows with the first column only
 template<typename T>
 inline void SQLiteWrapper::getRowsWithFirstColumn(sqlite3_stmt* preparedStatement, vector<T>& dbResults) {
     int columnsCount = -1;
@@ -286,6 +320,7 @@ inline void SQLiteWrapper::getRowsWithFirstColumn(sqlite3_stmt* preparedStatemen
     }
 }
 
+// method to finalize SQLite statement
 inline void SQLiteWrapper::finalize(sqlite3_stmt* preparedStatement) {
     const int ret = sqlite3_finalize(preparedStatement);
     ensureNonerror(preparedStatement, ret);
